668483ee9a1eaf38d2c95ce3091b724338b64b29
Derek Leach <derekleach84@gmail.com>
2018-07-26 09:14:13 +0100

    added some descriptions

--- lab_enumeration_callbacks_films_start/models/cinema.js
+++ lab_enumeration_callbacks_films_start/models/cinema.js
@@ -2,12 +2,16 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+/* map calls a provided callback function once for each element in an array,
+   in order, and constructs a new array from the results. */
 Cinema.prototype.filmTitles = function () {
   return this.films.map(function(film) {
     return film.title;
   })
 };
 
+/* The find method executes the callback function once for each index of the
+   array until it finds one where callback returns a true value */
 Cinema.prototype.findByTitle = function (){
   return this.films.find(function(film) {
     return film.title;
@@ -23,7 +27,12 @@ Cinema.prototype.getFilmsByGenre = function (genre) {
 };
 
 /* we pass in (year) we use the .some function, on films we then super duper
-equals on films.year and the year we pass in   */
+equals on films.year and the year we pass in */
+
+
+/* some() executes the callback function once for each element present in the array
+   until it finds one where callback returns a truthy value (a value that becomes true
+   when converted to a Boolean). */
 Cinema.prototype.filmsForYear = function (year) {
   return this.films.some(function(film){
     return film.year === year;
